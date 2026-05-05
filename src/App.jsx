2f311b7fe2977c6d0cba024ff5fd1a13d8313046
Sumon Debnath <debnathsumon1708@gmail.com>
2026-05-05 23:47:44 +0600

    Installed TailwindCss in the portfolio-v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div>Sumon Debnath!</div>;
+  return <div className="text-3xl font-mono text-sky-600">Sumon Debnath!</div>;
 }
 
 export default App;
